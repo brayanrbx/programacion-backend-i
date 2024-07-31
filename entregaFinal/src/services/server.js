17212b988cc85439642f9ff8aa96417556565af0
brayanrbx <brayan-rb@hotmail.com>
2024-07-30 22:13:05 -0500

    feat: :sparkles: Added new end points

---
+++ entregaFinal/src/services/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import routeMain from '../routes/routes.js';
+import views from '../routes/views.router.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extend: true }));
+app.use(express.static('./src/public'));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/views');
+
+
+app.use('/', views);
+app.use('/api', routeMain);
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const msg = err.message || 'Internal Server Error';
+    res.status(status).json({
+        msg,
+        stack: err.stack
+    })
+});
+
+export default app
